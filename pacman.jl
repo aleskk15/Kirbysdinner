330994899d7ee9dcb1e57f51dc48e1bacb901296
aleskk15 <A01738309@tec.mx>
2025-10-28 10:14:48 -0600

    Inicio de backend de julia

--- pacman.jl
+++ pacman.jl
@@ -1,46 +1,63 @@
-using Agents
+using Agents, Agents.Pathfinding
 
-#el arroba es una decoracion que viene con el paquete agents, le dice al compilador que hay codigo repetitivo e inserta el codigo repetittivo a la estructura
-@agent struct Ghost(GridAgent{2}) 
-    type::String = "Ghost"
-end
-const matrix = [
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0],
-  [0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0],
-  [0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0],
-  [0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0],
-  [0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0],
-  [0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0],
-  [0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0],
-  [0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0],
-  [0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0],
-  [0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0],
-  [0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0],
-  [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+matrix = [
+   0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0;
+   0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0;
+   0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0;
+   0 1 0 0 1 1 1 1 1 1 1 1 1 1 1 1 0;
+   0 1 0 0 1 1 1 1 1 1 1 1 1 1 1 1 0;
+   0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0;
+   0 1 1 1 1 1 1 1 1 1 1 0 0 1 1 1 0;  
+   0 1 1 1 1 1 1 1 1 1 1 0 0 1 1 1 0;
+   0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0;
+   0 1 1 1 0 0 1 1 1 1 1 1 1 1 1 1 0;
+   0 1 1 1 0 0 1 1 1 1 1 1 1 1 1 1 0;
+   0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0;
+   0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0;
+   0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
 ]
 
-#LLamas a la funcion con sus agentes, es como un self en python
+lab = BitArray(matrix)
+
+#El dos me dice las dimensiones del grid
+@agent struct Ghost(GridAgent{2})
+    type::String = "Ghost"
+end
+
+#por cada tic la función se llama, _agent es como el self de python
+# se va a mover con la ruta del pathfinder, por eso se pasa con el diccionario para poder acceder
+# Este es el agent_step! correcto
 function agent_step!(agent, model)
-    vecinos = [(1,0), (-1,0), (0,1), (0,-1)]
-    validos = []
-    for (dx, dy) in vecinos
-        nPos = (agent.pos[1] + dx, agent.pos[2] + dy) 
-        if  matrix[nPos[2]][nPos[1]] == 1          
-            push!(validos, nPos)
-        end
-    end
-    if !isempty(validos)
-        move_agent!(agent, rand(validos), model)
-    end
+    x = "llegue"
+    move_along_route!(agent, model, model.pathfinder)
+        if agent.pos == (5, 11)
+            plan_route!(agent, (4, 12), pathfinder)
+            move_along_route!(agent, model, model.pathfinder)
+        end
+        if agent.pos == (4, 12)
+            plan_route!(agent, (5, 11), pathfinder)
+            move_along_route!(agent, model, model.pathfinder)
+        end
+        end
+
+
+
+#Se hacen agentes y ambientes
+# Usamos el algoritmo estrella de los agentes 
+function initialize_model()
+    space = GridSpace((14,17); periodic=false, metric=:manhattan)
+    pathfinder = AStar(space; walkmap=lab, diagonal_movement= false)
+
+    properties = Dict(:pathfinder => pathfinder)
+    model = StandardABM(Ghost, space; agent_step!, properties)
+
+    add_agent!((2,2),Ghost, model)
+
+    plan_route!(model[1], (5,11), pathfinder)
+
+
+    return model, pathfinder
 end
 
-function initialize_model() 
-    space = GridSpace((17,14); periodic = false, metric = :manhattan) #distancia manhattan darle la vuelta a la manzana
-    model = StandardABM(Ghost, space; agent_step!) #los primeros son los atributos posicionales, : despues del ; son los parametros metricos
-    return model
-end
 
-model = initialize_model() #variables que son globales
-a = add_agent!(Ghost, pos=(2, 2), model) #le estoy diciendo que aparece un agente de tipo ghost y aparece en la posicion 3, 3
+model, pathfinder = initialize_model()
